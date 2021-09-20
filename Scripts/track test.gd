90e2057002a8b3ac80fa110e85e47abe05d795ac
jilhoe63 <82478256+jilhoe63@users.noreply.github.com>
2021-09-20 13:23:45 +1200

    Timer now starts

--- Scripts/track test.gd
+++ Scripts/track test.gd
@@ -17,3 +17,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_Area_body_entered(body):
+	if body.is_in_group("PlayerCar"):
+		Stopwatch.game_start()
